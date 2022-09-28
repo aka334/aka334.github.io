c2d253e8072e6eed1ba7004cf1474b5d12cbbea8
Aayush Karki <aka334>
2022-09-28 11:54:15 -0400

    Like color change and js formatting!

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-});
-
 var count = 0;
 function setColor(btn) {
   var property = document.getElementById(btn);
-  console.log(btn)
   if (count == 0) {
       property.style.color = "blue"
       count = 1;        
