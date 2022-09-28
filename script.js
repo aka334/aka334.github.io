66ed7c721027a2fa44835e63d125efd7fa3f5839
Aayush Karki <aka334>
2022-09-28 11:45:48 -0400

    Button color and link!

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ var myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
-})
+});
+
+var count = 0;
+function setColor(btn) {
+  var property = document.getElementById(btn);
+  console.log(btn)
+  if (count == 0) {
+      property.style.color = "blue"
+      count = 1;        
+  }
+  else {
+      property.style.color = "black"
+      count = 0;
+  }
+}
